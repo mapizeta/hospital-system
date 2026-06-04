dca0d149984d705f837125148884d02708c273a7
usuario-ignacio <ignacio.atuan2002@gmail.com>
2026-06-04 13:55:55 -0400

    Modificacion de Main para que funcione con patrones Singleton y Prototype

--- Main.java
+++ Main.java
@@ -2,10 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        HospitalConfig config1 = new HospitalConfig();
-        HospitalConfig config2 = new HospitalConfig();
+        HospitalConfigSingleton configGlobal = HospitalConfigSingleton.getInstance();
 
-        ICURoom room = new ICURoom();
+        ICURoomPrototype room = new ICURoomPrototype();
 
         LegacyMonitor monitor = new LegacyMonitor();
 
