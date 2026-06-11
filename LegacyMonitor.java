d8ed37c145cd851590c154377fe2382b91762a52
Crowpro1334 <italomartinez1654@gmail.com>
2026-06-11 13:11:35 -0400

    Update LegacyMonitor.java

--- LegacyMonitor.java
+++ LegacyMonitor.java
@@ -7,4 +7,30 @@ public class LegacyMonitor {
         System.out.println("Sending medical data");
     }
 }
-//Adapter
+class LegacyMonitorAdapter implements MedicalDevice {
+    private LegacyMonitor legacyMonitor;
+
+    public LegacyMonitorAdapter(LegacyMonitor legacyMonitor) {
+        this.legacyMonitor = legacyMonitor;
+    }
+
+    @Override
+    public void connect() {
+        legacyMonitor.establishConnection();
+    }
+
+    @Override
+    public void sendData() {
+        legacyMonitor.transmitInformation();
+    }
+}
+
+class Adapter {
+    public static void main(String[] args) {
+        LegacyMonitor legacyMonitor = new LegacyMonitor();
+        MedicalDevice monitorAdapter = new LegacyMonitorAdapter(legacyMonitor);
+
+        DeviceManager manager = new DeviceManager();
+        manager.connectDevice(monitorAdapter);
+    }
+}
