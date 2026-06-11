1ca89b7b22ec2b205675c7fa32d2c4d37690efd2
Crowpro1334 <italomartinez1654@gmail.com>
2026-06-11 13:01:59 -0400

    Update DeviceManager.java

--- DeviceManager.java
+++ DeviceManager.java
@@ -4,3 +4,32 @@ public class DeviceManager {
         device.sendData();
     }
 }
+
+class LegacyMonitorAdapter implements MedicalDevice {
+    private LegacyMonitor legacyMonitor;
+
+    public LegacyMonitorAdapter(LegacyMonitor legacyMonitor) {
+        this.legacyMonitor = legacyMonitor;
+    }
+
+    @Override
+    public void connect() {
+        legacyMonitor.establishConnection();
+    }
+
+    @Override
+    public void sendData() {
+        legacyMonitor.transmitInformation();
+    }
+}
+
+// Clase de demostración agregada
+class Adapter {
+    public static void main(String[] args) {
+        LegacyMonitor legacyMonitor = new LegacyMonitor();
+        MedicalDevice monitorAdapter = new LegacyMonitorAdapter(legacyMonitor);
+
+        DeviceManager manager = new DeviceManager();
+        manager.connectDevice(monitorAdapter);
+    }
+}
